42ff842ad818e987df792f3ae9c2f36cae993d07
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-11-27 13:41:51 +0100

    chore: bump swift-standards dependency to 0.4.0 for UInt8.Serializable

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-standards.git", from: "0.3.1"),
+        .package(url: "https://github.com/swift-standards/swift-standards.git", from: "0.4.0"),
     ],
     targets: [
         .target(
